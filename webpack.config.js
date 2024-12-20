e76bd0f1f01c064bf237f03a1e7b7573cc297726
000744210 <paulatkinson9111997@gmail.com>
2024-12-20 07:51:56 -0500

    published via package.json

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path')
 var webpack = require('webpack')
 //const CopyPlugin = require("copy-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
-let version = 56
+let version = 57
 let buildPath = "/dist/" + version + "/";
 module.exports = {
   entry: "./src/main.js",
